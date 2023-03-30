cea8247e7a81158f8084312b5fd4949447e0b64d
Matamala Daniel <daniel20mat@gmail.com>
2023-03-30 07:50:22 -0300

    protegiendo rutas

--- src/components/ProtecterRoute.jsx
+++ src/components/ProtecterRoute.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom'
 
 const permission = localStorage.getItem("permission");
 
-const ProtecterRoute = ({children, redirectTo='/'}) => {
+const ProtecterRoute = ({handlePermission, redirectTo='/'}) => {
 
-    if(!permission){
-    return <Navigate to={redirectTo} />
+  useEffect(() => {
+    console.log("se busco el permiso")
+    const savedPermission = localStorage.getItem("permission");
+    console.log(savedPermission, 'saved')
+    if (savedPermission) {
+      handlePermission(savedPermission);
     }
+  }, []);
 
-  return <Outlet/>
+   return  permission ? <Outlet /> : <Navigate to={redirectTo}/>
 }
 
 export default ProtecterRoute
